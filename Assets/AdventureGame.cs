4e9e83098143f090c5451a7796f147bc4c25b8f9
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-03-14 14:26:43 -0300

    Update, State, StateMachine in game scene

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State statingState;
+    // Start is called before the first frame update
+    State state; 
+    void Start()
+    {
+        state = statingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
