99c5046a306b11a00b2a68b1c52c78da4e509f47
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-03-16 17:07:09 -0300

    Finished Text Game & States => StateMateMachines

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+    [SerializeField] State statingState;
+    // int[] oddNumbers = {1,3,5,7,9};
+    // Start is called before the first frame update
+    State state; 
+    void Start()
+    {
+        state = statingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        //throw new NotImplementedException();
+        var nextStates = state.GetNextStates();
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        } 
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }
+
+        textComponent.text = state.GetStateStory();
+
+    }
+}
